6978a8add9e1da89793668ed9e46372551265580
Jhnvncntx <jhnvncntx@gmail.com>
2024-10-25 09:14:11 +0800

    Update staff.js

--- staff.js
+++ staff.js
@@ -37,30 +37,42 @@ function displayOrders(orders) {
         const orderElement = document.createElement('div');
         orderElement.className = 'orders';
         orderElement.innerHTML = `
-            <div class='orderId'>
-                Order: ${order.orderId}
-            </div>
-            <div class='customerName'>
-                Name: ${order.customerName}
-            </div>
-            <div class='totalAmount'>
-                Total: ${order.totalAmount}
+            <div class='orders-header'>
+                <div class='orderId'>
+                    Order: ${order.orderId}
+                </div>
+                <div class='customerName'>
+                    Name: ${order.customerName}
+                </div>
+                <div class='totalAmount'>
+                    Total: ₱${order.totalAmount}
+                </div>
             </div>
         `;
 
-        const itemList = document.createElement('ul');
         order.items.forEach(item => {
-            const itemElement = document.createElement('li');
-            itemElement.className = 'itemElement';
-            itemElement.innerHTML = `<div class='item'><div>Product: ${item.name}</div><div>Quantity: ${item.quantity}</div></div>`;
-            itemList.appendChild(itemElement);
+            const itemDiv = document.createElement('div');
+            itemDiv.className = 'itemDiv'
+            itemDiv.innerHTML = `
+            <div class='item-quantity'>${item.quantity}</div>
+            <div class='item-name'>${item.name}</div>
+            `;
+            orderElement.appendChild(itemDiv);
+            itemDiv.addEventListener('click', event => {
+                if (itemDiv.style.backgroundColor === 'lime') {
+                    itemDiv.style.backgroundColor = 'rgba(0, 0, 0, 0.5)'
+                    itemDiv.style.color = 'white'
+                } else {
+                itemDiv.style.backgroundColor = 'lime'
+                itemDiv.style.color = 'black'                    
+                }
+            })
         });
-
-        orderElement.appendChild(itemList);
 
         // Create "Ready" button
         if (order.status === 'Pending') {
             const readyButton = document.createElement('button');
+            readyButton.className = 'button'
             readyButton.innerText = 'Ready';
             readyButton.addEventListener('click', () => {
                 updateOrderStatus(order._id, 'Ready'); // Call the function to mark the order as Ready
@@ -68,23 +80,27 @@ function displayOrders(orders) {
             orderElement.appendChild(readyButton);
         }
 
+        // Create "Claimed" button for ready orders
+        if (order.status === 'Ready') {
+            const claimedButton = document.createElement('button');
+            claimedButton.className = 'button'
+            claimedButton.innerText = 'Claimed';
+            claimedButton.addEventListener('click', () => {
+                updateOrderStatus(order._id, 'Claimed'); // Call the function to mark the order as Claimed
+            });
+            orderElement.appendChild(claimedButton);
+        }        
+
         // Create "Cancel" button
         const cancelButton = document.createElement('button');
+        cancelButton.className = 'button'
         cancelButton.innerText = 'Cancel';
         cancelButton.addEventListener('click', () => {
             updateOrderStatus(order._id, 'Canceled'); // Call the function to cancel the order
         });
         orderElement.appendChild(cancelButton);
 
-        // Create "Claimed" button for ready orders
-        if (order.status === 'Ready') {
-            const claimedButton = document.createElement('button');
-            claimedButton.innerText = 'Claimed';
-            claimedButton.addEventListener('click', () => {
-                updateOrderStatus(order._id, 'Claimed'); // Call the function to mark the order as Claimed
-            });
-            orderElement.appendChild(claimedButton);
-        }
+
 
         // Append orderElement to the appropriate container based on its status
         if (order.status === 'Pending') {
