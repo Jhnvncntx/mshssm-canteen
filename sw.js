c84fa47128ae92e80cb0f77381f6d022a88c8f34
Jhnvncntx <jhnvncntx@gmail.com>
2024-10-25 17:55:33 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+// Inside firebase-messaging-sw.js or sw.js
+// self.addEventListener('push', event => {
+//   const data = event.data.json();
+//   console.log('Push received:', data);
+
+//   const options = {
+//     body: data.body,
+//     icon: 'bg-image.jpg',  // Replace with your icon URL
+//   };
+
+//   // Show the notification
+//   event.waitUntil(
+//     self.registration.showNotification(data.title, options)
+//   );
+// });
+
+
+self.addEventListener('push', function(event) {
+  const data = event.data ? event.data.json() : {};
+
+  const options = {
+    body: data.body || 'Default push message body',
+    icon: 'bg-image.jpg',
+    badge: 'path/to/badge.png',
+  };
+
+  event.waitUntil(
+    self.registration.showNotification(data.title || 'Default Title', options)
+  );
+});
